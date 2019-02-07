ee15f810c6563639129c10ea9523afc34362747d
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-02-06 22:57:32 -0600

    Update schedule_all.sh

--- schedule_all.sh
+++ schedule_all.sh
@@ -8,7 +8,8 @@ grep "NOAA 15" /home/pi/weather/predict/weather.txt -A 2 > /home/pi/weather/pred
 grep "NOAA 18" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 grep "NOAA 19" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 grep "METEOR-M 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
-#Meteor M N2-2 launch delayed to NET June 2019
+# Meteor M N2-2 launch delayed to NET June 2019
+# grep "METEOR-N 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 
 #Remove all AT jobs
 
@@ -17,8 +18,9 @@ for i in `atq | awk '{print $1}'`;do atrm $i;done
 #http://phqfh.co.uk/status.htm (check for changes) 02/06/2019
 #Schedule Satellite Passes:
 
-/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.100
+/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.1000
 /home/pi/weather/predict/schedule_satellite.sh "NOAA 18" 137.9125
 /home/pi/weather/predict/schedule_satellite.sh "NOAA 15" 137.6200
-/home/pi/weather/predict/schedule_satellite.sh "METEOR-M 2" 137.900
-#Meteor M N2-2 launch delayed to NET June 2019
+/home/pi/weather/predict/schedule_satellite.sh "METEOR-M 2" 137.9000
+# Meteor M N2-2 launch delayed to NET June 2019
+# /home/pi/weather/predict/schedule_satellite.sh "METEOR-N 2" xxx.xxxx
