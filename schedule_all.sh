5e498aa2a24cbd7054408630045ebe3f021609a6
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-02-06 22:54:18 -0600

    Update schedule_all.sh

--- schedule_all.sh
+++ schedule_all.sh
@@ -8,15 +8,17 @@ grep "NOAA 15" /home/pi/weather/predict/weather.txt -A 2 > /home/pi/weather/pred
 grep "NOAA 18" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 grep "NOAA 19" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 grep "METEOR-M 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
-
+#Meteor M N2-2 launch delayed to NET June 2019
 
 #Remove all AT jobs
 
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 
-
+#http://phqfh.co.uk/status.htm (check for changes) 02/06/2019
 #Schedule Satellite Passes:
 
-/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.1000
+/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.100
 /home/pi/weather/predict/schedule_satellite.sh "NOAA 18" 137.9125
 /home/pi/weather/predict/schedule_satellite.sh "NOAA 15" 137.6200
+/home/pi/weather/predict/schedule_satellite.sh "METEOR-M 2" 137.900
+#Meteor M N2-2 launch delayed to NET June 2019
