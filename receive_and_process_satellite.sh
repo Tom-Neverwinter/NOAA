006d4feac9a2dfabca4cfc898208cd6868d72bbe
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-02-06 23:13:46 -0600

    Update receive_and_process_satellite.sh

--- receive_and_process_satellite.sh
+++ receive_and_process_satellite.sh
@@ -12,13 +12,25 @@ sudo timeout $6 rtl_fm -f ${2}M -s 60k -g 45 -p 55 -E wav -E deemp -F 9 - | sox 
 PassStart=`expr $5 + 90`
 
 if [ -e $3.wav ]
-  then
-    /usr/local/bin/wxmap -T "${1}" -H $4 -p 0 -l 0 -o $PassStart ${3}-map.png
+then
+/usr/local/bin/wxmap -T "${1}" -H $4 -p 0 -l 0 -o $PassStart ${3}-map.png
 
-    /usr/local/bin/wxtoimg -m ${3}-map.png -e ZA $3.wav $3.png
+/usr/local/bin/wxtoimg -m ${3}-map.png -e ZA $3.wav $3.png
+/usr/local/bin/wxtoimg -m ${3}-map.png -e NO $3.wav ${3}.NO.png
+/usr/local/bin/wxtoimg -m ${3}-map.png -e MCIR $3.wav ${3}.MCIR.png
+/usr/local/bin/wxtoimg -m ${3}-map.png -e MSA $3.wav ${3}.MSA.png
 
-    sleep 60
+# Delete old satellite image files from www directory
+sudo rm /var/www/html/NOAA/*.*
 
-    /home/pi/weathertweeter/weathertweeter.py ${3}.png ${3}
+# Copy, rename current files to www directory
+sudo cp /home/pi/weather/NOAA*.png /var/www/html/NOAA/current.png
+sudo cp /home/pi/weather/NOAA*.NO.png /var/www/html/NOAA/current.NO.png
+sudo cp /home/pi/weather/NOAA*.MCIR.png /var/www/html/NOAA/current.MCIR.png
+sudo cp /home/pi/weather/NOAA*.MSA.png /var/www/html/NOAA/current.MSA.png
+
+# Delete old .png and .wav files from weather directory
+sudo rm /home/pi/weather/*.png
+sudo rm /home/pi/weather/*.wav
 
 fi
